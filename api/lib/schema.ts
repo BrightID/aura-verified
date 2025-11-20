c086f1187bde637974102c6b4b1eddd7b0c7e6be
Ali Maktabi <maktabi876@gmail.com>
2025-11-21 02:36:38 +0330

    feat: update database schema

--- api/lib/schema.ts
+++ api/lib/schema.ts
@@ -16,11 +16,30 @@ export const projectsTable = pgTable('projects', {
   name: varchar({ length: 255 }).notNull(),
   description: varchar({ length: 255 }).notNull(),
   requirementLevel: integer().notNull(),
-  deadline: timestamp(),
-  createdAt: timestamp().defaultNow(),
-  updatedAt: timestamp().default(sql`CURRENT_TIMESTAMP`),
   isActive: boolean().notNull().default(true),
   image: varchar({ length: 1000 }),
   landingMarkdown: text(),
-  creatorId: varchar({ length: 43 }).references(() => usersTable.id)
+  creatorId: varchar({ length: 43 }).references(() => usersTable.id),
+  logoUrl: varchar({ length: 1000 }),
+  websiteUrl: varchar({ length: 1000 }),
+  remainingtokens: integer().default(0),
+  selectedPlanId: integer().references(() => verificationPlansTable.id),
+  brightIdAppId: varchar({ length: 500 }),
+  deadline: timestamp(),
+
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().default(sql`CURRENT_TIMESTAMP`)
+})
+
+export const verificationPlansTable = pgTable('plans', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  isActive: boolean().default(true),
+  tokens: integer().default(100),
+  pricePerExcess: integer().default(1),
+  description: text(),
+  isRecommended: boolean(),
+  order: integer().default(0),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().default(sql`CURRENT_TIMESTAMP`)
 })
