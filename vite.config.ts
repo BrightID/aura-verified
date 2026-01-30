fbbac4e7b791f5f7f6210f4951b7328b44ff17fa
Bob six <maktabi876@gmail.com>
2026-01-30 15:15:19 +0330

    feat: add story book and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { litHMRPlugin } from './vite-plugin-lit-hmr'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { litHMRPlugin } from './vite-plugin-lit-hmr';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   root: '.',
   build: {
@@ -17,14 +24,14 @@ export default defineConfig({
       '^/auranode(/.*)?$': {
         target: 'https://aura-node.brightid.org',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/auranode/, ''),
+        rewrite: path => path.replace(/^\/auranode/, ''),
         secure: process.env.NODE_ENV?.toLowerCase() !== 'development'
       },
-
       '^/auranode-test(/.*)?$': {
         target: 'https://aura-test.brightid.org',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/auranode-test/, ''), // Fixed regex
+        rewrite: path => path.replace(/^\/auranode-test/, ''),
+        // Fixed regex
         secure: process.env.NODE_ENV?.toLowerCase() !== 'development'
       },
       '^/api(/.*)?$': {
@@ -35,4 +42,69 @@ export default defineConfig({
     }
   }
   // assetsInclude: ['**/*.html']
-})
+  ,
+
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['packages/.storybook/vitest.setup.ts']
+      }
+    }, {
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['packages/ui/.storybook/vitest.setup.ts']
+      }
+    }, {
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['packages/ui/.storybook/vitest.setup.ts']
+      }
+    }]
+  }
+});
