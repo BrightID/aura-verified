77731c23f5217989fa2beea7e4ffb1270a22d47a
Bob six <maktabi876@gmail.com>
2026-04-03 13:06:59 +0330

    fix: support cors for getting projects

--- api/projects.ts
+++ api/projects.ts
@@ -2,6 +2,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 import { db } from './lib/db.js'
 import { projectsTable } from './lib/schema.js'
 import { eq } from 'drizzle-orm'
+import withCors from './lib/cors.js'
 
 const handler = async (req: VercelRequest, res: VercelResponse) => {
   const projects = await db.select().from(projectsTable).where(eq(projectsTable.isActive, true))
@@ -9,4 +10,4 @@ const handler = async (req: VercelRequest, res: VercelResponse) => {
   res.send(projects)
 }
 
-export default handler
+export default withCors(handler)
